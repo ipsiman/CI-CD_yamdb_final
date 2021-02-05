9b48c4fd2e733357360b4b287427ca0f2001c86f
Sergei Pavlenko <dezix@yandex.ru>
2021-02-05 20:20:17 +0300

    fix pep8

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -86,7 +86,6 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -104,7 +103,6 @@ DATABASES = {
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
 
 
 # Password validation
